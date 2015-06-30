5304cf391fe4dc0b655da001d3db74a27b707c49
ghstahl@gmail.com <ghstahl@gmail.com>
2015-06-30 11:36:12 -0700

    Parser assistant interface. Uses to help the parser engines determine what to do with a proposed option name

---
+++ FluentCommandLineParser/Internals/Parsing/IParserAssistant.cs
@@ -0,0 +1,15 @@
+﻿namespace Fclp.Internals.Parsing
+{
+    /// <summary>
+    /// Parser assistant, lets the user indicate information about options as they are being parsed
+    /// </summary>
+    public interface IParserAssistant
+    {
+        /// <summary>
+        /// Indicates that this option is in our optionName list
+        /// </summary>
+        /// <param name="optionName"></param>
+        /// <returns></returns>
+        bool IsBeingWatchedFor(string optionName);
+    }
+}
